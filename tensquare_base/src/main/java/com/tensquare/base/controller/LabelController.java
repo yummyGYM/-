b05cda569346901ff97fd207d25303571cc0a60d
yummy_gym <yummy_gym@163.com>
2019-09-15 10:54:15 +0800

    初始化项目

--- tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
+++ tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
@@ -1,6 +1,8 @@
 package com.tensquare.base.controller;
 
+import com.tensquare.base.pojo.Label;
 import entity.Result;
+import entity.StatusCode;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -14,9 +16,25 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/label")
 public class LabelController {
 
-    @GetMapping("")
+    @RequestMapping(method = RequestMethod.GET)
     public Result findAll(){
-        return new Result();
+        return new Result(true, StatusCode.OK, "查询成功");
     }
+
+    @RequestMapping(value = "/{labelId}", method = RequestMethod.GET)
+    public Result findById(@PathVariable("labelId") String labelId){
+        return new Result(true, StatusCode.OK, "查询成功");
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Result save(@RequestBody Label label){
+        return new Result(true, StatusCode.OK, "添加成功");
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    public Result deleteById(@PathVariable("labelId") String labelId){
+        return new Result(true, StatusCode.OK, "删除成功");
+    }
+
 
 }
---
+++ tensquare_base/src/main/java/com/tensquare/base/pojo/Label.java
@@ -0,0 +1,10 @@
+package com.tensquare.base.pojo;
+
+/**
+ * @Author: Gu Yaming
+ * @Description:
+ * @Date:Create：in 2019/9/15 9:52
+ * @Modified By：
+ */
+public class Label {
+}
